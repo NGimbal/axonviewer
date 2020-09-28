140b70722303224aa67356eee64107da1cdb59d9
NGimbal <nicolas.p.schmidt@gmail.com>
2020-09-28 08:50:58 -0400

    finished up hover

--- axonViewer.js
+++ axonViewer.js
@@ -11,7 +11,9 @@ THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
 let renderer, aspect, scene, camera, controls, sceneBox;
 
 // For hover / selection
-let raycaster, mouse;
+let raycaster, mouse, selection;
+
+let outlineMat = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.BackSide, depthWrite: false } );
 
 // Initialize scene
 function init() {
@@ -41,6 +43,10 @@ function init() {
   // For hover / selection                                 
   raycaster = new THREE.Raycaster;
   mouse = new THREE.Vector2();
+  selection = [];
+  // selection = new THREE.Object3D();
+  // selection.visible = false;
+  // scene.add(selection);
 
   controls.update();
                                       
@@ -49,15 +55,18 @@ function init() {
 
   // Lighting
   {				
-    scene.add( new THREE.AmbientLight( 0xFFEDBA, 1 ) );
+    scene.add( new THREE.AmbientLight( 0xFFEDBA, 0.1 ) );
     
-    const dirLight = new THREE.DirectionalLight(0xFFFFFF, 2);
-    dirLight.position.set(250, 250, 2000);
-    dirLight.lookAt(0,0,0);
-    scene.add(dirLight);
+    // const dirLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+    // dirLight.position.set(250, 250, 2000);
+    // dirLight.lookAt(0,0,0);
+    // scene.add(dirLight);
 
-    let hemiLight = new THREE.HemisphereLight( 0xffffbb, 0x5544BB, 1 );
-    scene.add( hemiLight );
+    // let hemiLight = new THREE.HemisphereLight( 0xffffbb, 0x5544BB, 0.4 );
+    // scene.add( hemiLight );
+    var light = new THREE.HemisphereLight( 0xffffff, 0x080808, 1 );
+    light.position.set( - 10.25, 10, 10.25 );
+    scene.add( light );
   }
 
   // Load Rhino File
@@ -68,26 +77,35 @@ function init() {
     console.log(object);
     console.log(object.children.length);
     // Set Bounding Box
-    scene.add( object );
 
     for(let i = 0; i < object.children.length; i++){
+
+      // Set Material from Rhino Display Color
       let col = object.children[i].userData.attributes.objectColor;
-      let mat = new THREE.MeshStandardMaterial({transparent:true,opacity:0.5});
-     
-      mat.color = col;
-      
+
+      let mat = new THREE.MeshLambertMaterial( {
+        color: new THREE.Color(col.r/255, col.g/255, col.b/255),
+      } );
+
+      // Recenter geometry at Object3D bounding box
+      let box = new THREE.Box3().setFromObject(object.children[i]);
+      box.getCenter(object.children[i].position);
+
       for (let m of object.children[i].children){
         m.material = mat;
+        
+        box.getCenter(m.position);
+        m.position.multiplyScalar(-1);
       }
     }
-
+    scene.add( object );
     sceneBox.setFromObject(object);
-
     zoomToScene();
   });
   
   // Hover
   document.querySelector("#c").addEventListener('mousemove', mouseMove, false);
+  document.querySelector("#c").addEventListener('mouseup', mouseUp, false);
   requestAnimationFrame(render);
 }
 
@@ -106,28 +124,38 @@ function render(time) {
     camera.bottom = side * -1;
     camera.top = side;
 
-
     camera.updateProjectionMatrix();
     controls.update();
   }
 
   raycaster.setFromCamera(mouse, camera);
-// console.log(scene.children[4]);
+
   if(scene.children[4]){
     let intersects = raycaster.intersectObjects(scene.children, true);
     
-    if(intersects.length > 1){
+    if(intersects.length > 0 ){
+
       let hover = intersects[0].object;
 
-      if(hover.type == "Mesh"){
-        let parent = hover.parent;
+      scene.traverse((obj) => {
+        if(obj !== hover){
+          if(obj.material){
+            obj.material.emissiveIntensity = 0.0;
+          }
+        }
+      })
 
-        for(let i = 0; i < parent.children.length; i++){
-          parent.children[i].material.color = new THREE.Color(0xff0000);
+      if(hover.type == "Mesh"){
+        // Get parent of mesh
+        let parent = hover.parent;
+        let copy = parent.clone(true);
+
+        for(let c of copy.children){
+          c.material.emissive.set(new THREE.Color(0xffeeee));
+          c.material.emissiveIntensity = 0.65;
         }
       }
     }
-
   }
   
 
@@ -173,6 +201,10 @@ function mouseMove(e){
 
   mouse.x = ( e.offsetX / canvas.clientWidth ) * 2 - 1;
   mouse.y = - ( e.offsetY / canvas.clientHeight ) * 2 + 1;
+}
+
+function mouseUp(e){
+  
 }
 
 init();
