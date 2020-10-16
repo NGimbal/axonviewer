37718b1b8b532c84a072a2ae4d996237f0a677f6
NGimbal <nicolas.p.schmidt@gmail.com>
2020-10-16 00:39:07 -0400

    explosions

--- contextMenu.js
+++ contextMenu.js
@@ -61,7 +61,9 @@ function setMenuState(obj){
   document.querySelector('#Y').value = obj.userData.explode.vec.y.toFixed(2);
   document.querySelector('#Z').value = obj.userData.explode.vec.z.toFixed(2);
   document.querySelector('#maxExplode').value = obj.userData.explode.max;
-  // document.querySelector('#modelColor').value = 
+
+  let col = obj.userData.attributes.drawColor;
+  document.querySelector('#modelColor').value = rgbToHex(col.r, col.g, col.b);
 }
 
 
@@ -76,11 +78,66 @@ function main(){
   window.addEventListener("mouseup", endDrag);
   //------ open, drag, close ------
 
-  // document.querySelector('#X').addEventListener('input', changeX);
-  // document.querySelector('#Y').addEventListener('input', changeY);
-  // document.querySelector('#Z').addEventListener('input', changeZ);
-  // document.querySelector('#maxExplode').addEventListener('input', changeMaxExplode);
-  // document.querySelector('#modelColor').addEventListener('input', changeColor);
+  document.querySelector('#X').addEventListener('input', changeX);
+  document.querySelector('#Y').addEventListener('input', changeY);
+  document.querySelector('#Z').addEventListener('input', changeZ);
+  document.querySelector('#maxExplode').addEventListener('input', changeMaxExplode);
+  document.querySelector('#modelColor').addEventListener('input', changeColor);
+}
+
+function changeX(e){
+  console.log("hi");
+  let val = parseFloat(e.target.value);
+  selection.map(obj => obj.userData.explode.vec.setX(val).normalize());
+}
+
+function changeY(e){
+  let val = parseFloat(e.target.value);
+  selection.map(obj => obj.userData.explode.vec.setY(val).normalize());
+}
+
+function changeZ(e){
+  let val = parseFloat(e.target.value);
+  selection.map(obj => obj.userData.explode.vec.setZ(val).normalize());
+}
+
+function changeMaxExplode(e){
+  let val = parseInt(e.target.value);
+  selection.map(obj => obj.userData.explode.max = val);
+}
+
+function changeColor(e){
+  let val = e.target.value;
+
+  selection.map(a => a.traverse((b) => {
+      if(b.material) {
+        b.material.color.set(val);
+      }
+      if(b.userData.attributes){
+        b.userData.attributes.drawColor = hexToRgb(val);
+      }
+      return b;
+    })
+  );
+}
+
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
 }
 
 main();
